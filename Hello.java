b71c6d91396cea3d334e9f4975274decbe8db5d3
Karuita <karuita@gmail.com>
2013-04-22 23:01:25 -0700

    setting up

---
+++ Hello.java
@@ -0,0 +1,25 @@
+//
+//  Hello.java
+//  
+//
+//  Created by Lynn on 4/10/13.
+//  Copyright 2013 __MyCompanyName__. All rights reserved.
+//
+
+public class Hello {
+	
+	public static void main(String[] args){
+		System.out.println("Hello World!");
+		
+		System.out.print("Radius : ");
+		java.util.Scanner scanner = new java.util.Scanner(System.in);
+		int radius = scanner.nextInt();
+		
+		System.out.print("Height : ");
+		scanner = new java.util.Scanner(System.in);
+		int height = scanner.nextInt();
+		
+		double volume = (1.0/3.0) * Math.PI * radius * radius * height;
+		System.out.println(volume);
+	}
+}
